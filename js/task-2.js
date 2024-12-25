887078d096cdd49a82037720424b18199bdb01bf
dreamer981 <sdreamer590@gmail.com>
2024-12-25 20:36:49 +0300

    task 2 düzeltildi

--- js/task-2.js
+++ js/task-2.js
@@ -1,47 +1,34 @@
 const images = [
-    {
-      url: "images/1.png",
-      alt: "Working",
-    },
-    {
-      url: "images/2.png",
-      alt: "New Idea",
-    },
-    {
-      url: "images/3.png",
-      alt: "Develop experience",
-    },
-    {
-      url: "images/4.png",
-      alt: "Meeting",
-    },
-    {
-      url: "images/5.png",
-      alt: "Rest time",
-    },
-    {
-      url: "images/6.png",
-      alt: "Working space",
-    },
-  ];
-  
-  // Gallery öğesini seç
-  const gallery = document.querySelector('.gallery');
-  
-  // Resim öğelerini oluştur ve galeriyi doldur
-  images.forEach(image => {
-    // Yeni <li> öğesi oluştur
-    const li = document.createElement('li');
-    
-    // Yeni <img> öğesi oluştur
-    const img = document.createElement('img');
-    img.src = image.url; // Resim URL'si
-    img.alt = image.alt; // Resim alt metni
-  
-    // <li> öğesine <img> öğesini ekle
-    li.appendChild(img);
-    
-    // <li> öğesini galeriye ekle
-    gallery.appendChild(li);
-  });
-  
+  {
+    url: "images/1.png",
+    alt: "Working",
+  },
+  {
+    url: "images/2.png",
+    alt: "New Idea",
+  },
+  {
+    url: "images/3.png",
+    alt: "Develop experience",
+  },
+  {
+    url: "images/4.png",
+    alt: "Meeting",
+  },
+  {
+    url: "images/5.png",
+    alt: "Rest time",
+  },
+  {
+    url: "images/6.png",
+    alt: "Working space",
+  },
+];
+
+const gallery = document.querySelector(".gallery");
+
+const galleryMarkup = images
+  .map((image) => `<li><img src="${image.url}" alt="${image.alt}"></li>`)
+  .join("");
+
+gallery.innerHTML = galleryMarkup;
